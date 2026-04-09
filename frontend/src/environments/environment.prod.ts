896099f475d7dc9f5a1f6254631625d2408db37d
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-04-09 16:57:16 -0500

    hol

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Esta URL la debes cambiar por la URL real que te asigne tu servidor en Railway o tu nube
   // (sin el slash final)
-  apiUrl: 'https://tu-dominio-en-railway.up.railway.app'
+  apiUrl: 'https://gestortareas-production-e356.up.railway.app'
 };

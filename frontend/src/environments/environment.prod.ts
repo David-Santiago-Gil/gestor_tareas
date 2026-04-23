8936708058ca77904bcc3f396f7cadca177178e8
David-Santiago-Gil <gilsantiagodepa@gmail.com>
2026-04-23 15:01:54 -0500

    Fix de tipado asincrono MySQL para visualizacion en Vercel

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Esta URL la debes cambiar por la URL real que te asigne tu servidor en Railway o tu nube
   // (sin el slash final)
-  apiUrl: 'https://gestortareas-production-e356.up.railway.app'
+  apiUrl: 'https://gestor-tareas-api-0610.onrender.com'
 };
